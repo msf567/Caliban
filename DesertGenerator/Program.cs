e03933b2327a832c918e3b2c915aaaee23272de0
msf567 <msf567@gmail.com>
2019-02-10 06:43:06 +0000

    init

--- DesertGenerator/Program.cs
+++ DesertGenerator/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Media;
 using System.Threading;
+using System.Xml.Schema;
 
 
 namespace DesertGenerator
@@ -22,43 +24,59 @@ namespace DesertGenerator
         };
 
         private static DirectoryInfo _desertRoot = new DirectoryInfo("D:\\Desert");
-        private static MusicPlayer _music = new MusicPlayer();
+        private static SoundPlayer _music = new SoundPlayer();
 
         public static void Main(string[] args)
         {
-
+           
             DrawMenu();
+            
+            while(true){}
         }
 
         private static void DrawMenu()
         {
+            Console.SetWindowSize(122,24);
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+            _music.SoundLocation = "desert.wav";
+            _music.Load();
+            
+            Thread.Sleep(2000);
+            
+            _music.Play();
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Thread.Sleep(500);
-            Console.WriteLine(
-                "███████╗ ██████╗ ██████╗ ███████╗ █████╗ ██╗  ██╗███████╗███╗   ██╗    ██████╗ ██╗   ██╗███╗   ██╗███████╗███████╗    ");
-            Thread.Sleep(500);
-            Console.WriteLine(
-                "██╔════╝██╔═══██╗██╔══██╗██╔════╝██╔══██╗██║ ██╔╝██╔════╝████╗  ██║    ██╔══██╗██║   ██║████╗  ██║██╔════╝██╔════╝    ");
-            Thread.Sleep(500);
-            Console.WriteLine(
-                "█████╗  ██║   ██║██████╔╝███████╗███████║█████╔╝ █████╗  ██╔██╗ ██║    ██║  ██║██║   ██║██╔██╗ ██║█████╗  ███████╗    ");
-            Thread.Sleep(500);
-            Console.WriteLine(
-                "██╔══╝  ██║   ██║██╔══██╗╚════██║██╔══██║██╔═██╗ ██╔══╝  ██║╚██╗██║    ██║  ██║██║   ██║██║╚██╗██║██╔══╝  ╚════██║    ");
-            Thread.Sleep(500);
-            Console.WriteLine(
-                "██║     ╚██████╔╝██║  ██║███████║██║  ██║██║  ██╗███████╗██║ ╚████║    ██████╔╝╚██████╔╝██║ ╚████║███████╗███████║    ");
-            Thread.Sleep(500);
-            Console.WriteLine(
-                "╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝    ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚══════╝╚══════╝    ");
-            Thread.Sleep(500);
-            Console.WriteLine(
-                "                                                                                                                      ");
-            Thread.Sleep(500);
+            
+            Console.WriteLine(Center("~~~"));
+            Console.WriteLine(Center("Mark Fingerhut Presents"));
+            Console.WriteLine(Center("~~~"));
+            Thread.Sleep(2398);
+            Console.WriteLine(Center("A FileSystem Survival Game"));
+            Console.WriteLine(Center("~~~"));
+            Thread.Sleep(2412);
+            Console.WriteLine(Center(" ▄▀▀▀█▄    ▄▀▀▀▀▄   ▄▀▀▄▀▀▀▄  ▄▀▀▀▀▄  ▄▀▀█▄   ▄▀▀▄ █  ▄▀▀█▄▄▄▄  ▄▀▀▄ ▀▄      ▄▀▀▀▀▄  ▄▀▀█▄   ▄▀▀▄ ▀▄  ▄▀▀█▄▄   ▄▀▀▀▀▄ "));
+            Thread.Sleep(594);
+            Console.WriteLine(Center("█  ▄▀  ▀▄ █      █ █   █   █ █ █   ▐ ▐ ▄▀ ▀▄ █  █ ▄▀ ▐  ▄▀   ▐ █  █ █ █     █ █   ▐ ▐ ▄▀ ▀▄ █  █ █ █ █ ▄▀   █ █ █   ▐ "));
+            Thread.Sleep(594);
+            Console.WriteLine(Center("▐ █▄▄▄▄   █      █ ▐  █▀▀█▀     ▀▄     █▄▄▄█ ▐  █▀▄    █▄▄▄▄▄  ▐  █  ▀█        ▀▄     █▄▄▄█ ▐  █  ▀█ ▐ █    █    ▀▄   "));
+            Thread.Sleep(594);
+            Console.WriteLine(Center(" █    ▐   ▀▄    ▄▀  ▄▀    █  ▀▄   █   ▄▀   █   █   █   █    ▌    █   █      ▀▄   █   ▄▀   █   █   █    █    █ ▀▄   █  "));
+            Thread.Sleep(594);
+            Console.WriteLine(Center(" █          ▀▀▀▀   █     █    █▀▀▀   █   ▄▀  ▄▀   █   ▄▀▄▄▄▄   ▄▀   █        █▀▀▀   █   ▄▀  ▄▀   █    ▄▀▄▄▄▄▀  █▀▀▀   "));
+            Thread.Sleep(594);
+            Console.WriteLine(Center("█                  ▐     ▐    ▐      ▐   ▐   █    ▐   █    ▐   █    ▐        ▐      ▐   ▐   █    ▐   █     ▐   ▐      "));
+            Thread.Sleep(594);
+            Console.WriteLine(Center("▐                                            ▐        ▐        ▐                            ▐        ▐                "));
+            Thread.Sleep(594);
             Console.ForegroundColor = ConsoleColor.White;
         }
 
+        private static string Center(string s)
+        {
+            int ConsoleWidth = Console.WindowWidth;
+            int sLen = s.Length;
+            int gap = ConsoleWidth - sLen;
+            return new string(' ',gap/2) + s;
+        }
         private static void ClearDesert()
         {
             foreach (var rock in HeavyRocks)
