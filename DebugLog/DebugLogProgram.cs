6fa9b57d13ae384c68e08858519b87dfa808c354
Mark <msf567@gmail.com>
2019-02-16 01:07:07 +0000

    Debug Log and Game

---
+++ DebugLog/DebugLogProgram.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using Caliban.Transport;
+
+namespace DebugLog
+{
+    internal class DebugLogProgram
+    {
+        class DebugLog : ClientApp
+        {
+            private bool closeFlag;
+
+            public DebugLog() : base("DEBUG")
+            {
+                Thread t = new Thread(UpdateThread);
+                t.Start();
+            }
+
+            private void WriteLine(string s)
+            {
+                Console.WriteLine(s);
+            }
+
+            private void UpdateThread()
+            {
+                while (!closeFlag)
+                {
+                    Thread.Sleep(100);
+                }
+            }
+
+            protected override void ClientOnMessageReceived(byte[] message)
+            {
+                Message m = Messages.Parse(message);
+                switch (m.Type)
+                {
+                    case MessageType.GAME_CLOSE:
+                        closeFlag = true;
+                        break;
+                    case MessageType.DEBUG_LOG:
+                        WriteLine(Messages.Parse(message).Param);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            DebugLog d = new DebugLog();
+        }
+    }
+}
