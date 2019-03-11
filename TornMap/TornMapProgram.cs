58fc11626c284ffc7e84eb2a3a279f4b262e7540
Mark Fingerhut <msf567@gmail.com>
2019-03-11 08:57:03 -0400

    more audio

---
+++ TornMap/TornMapProgram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TornMap
+{
+    internal static class TornMapProgram
+    {
+        public static void Main(string[] _args)
+        {
+            Console.WriteLine("A Map!");
+            Console.ReadLine();
+        }
+    }
+}
